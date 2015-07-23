d8a8bf9f676df1599a90efe62f8a2bdaf8e76b71
Eric Katz <ericpkatz@gmail.com>
2015-07-23 01:18:34 +0000

    use express router

---
+++ app/routers/things.js
@@ -0,0 +1,91 @@
+var express = require("express");
+var Thing = require("../models/thing");
+
+var app = express.Router();
+
+function findThingById(req, res, next){
+    res.locals.activePath = "/things";
+    res.locals.title = "Add A New Thing";
+    if(req.params.id){
+        Thing.findById(req.params.id, function(err, thing){
+               if(err)
+                    next(err);
+               else{
+                   res.locals.thing = thing; 
+                   res.locals.title = "Editing " + thing.name;
+                   next();
+               }
+            });
+    }
+    else{
+        res.locals.thing = new Thing(); 
+        next();
+    }
+        
+}
+
+app.get("/", function(req, res){
+    Thing.find({}).then(function(things){
+       res.render("things", {
+           title: "Things",
+           activePath: "/things",
+           things: things
+       });
+    });
+});
+
+app.post("/new", function(req, res){
+   var thing = new Thing(req.body); 
+   thing.save(function(err){
+       if(!err){
+         req.flash("info", "A thing with an id of " + thing._id + " has been inserted");
+         res.redirect("/things"); 
+       }
+       else{
+            res.render("thing", {
+                thing: new Thing(),
+                activePath: "/things",
+                title: "Insert a New Thing",
+                error: err
+            });
+       }
+    });
+});
+
+app.post("/:id/delete", function(req, res, next){
+    Thing.remove({_id: req.params.id})
+        .then(function(){
+            req.flash("info", "A thing has been deleted")
+            res.redirect("/things");
+        });
+    
+});
+app.post("/:id", findThingById, function(req, res){
+            var thing = res.locals.thing;
+            console.log(req.body);
+            thing.name = req.body.name;
+            thing.active = req.body.active;
+            thing.save(function(err, _thing){
+                if(!err){
+                    req.flash("info", "A Thing with the name " + thing.name + " has been saved");
+                    res.redirect("/things"); 
+                }
+                else {
+                    res.render("thing", {
+                       error: err,
+                       thing: thing,
+                       title: "Thing " + thing.name
+                    });  
+                }
+            });
+});
+
+app.get("/new", findThingById, function(req, res){
+    res.render("thing");
+    
+});
+app.get("/:id", findThingById, function(req, res){
+    res.render("thing");  
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var session = require("express-session");
 var cookieParser = require("cookie-parser");
 var flash = require("express-flash");
 
+var thingsRouter = require("./app/routers/things")
+
 db.connect()
     .then(function(){
         console.log("connected");
@@ -51,87 +53,7 @@ app.get("/people", function(req, res){
    });
 });
 
-function findThingById(req, res, next){
-    res.locals.activePath = "/things";
-    res.locals.title = "Add A New Thing";
-    if(req.params.id){
-        Thing.findById(req.params.id, function(err, thing){
-               if(err)
-                    next(err);
-               else{
-                   res.locals.thing = thing; 
-                   res.locals.title = "Editing " + thing.name;
-                   next();
-               }
-            });
-    }
-    else{
-        res.locals.thing = new Thing(); 
-        next();
-    }
-        
-}
+app.use("/things", thingsRouter);
 
-app.get("/things", function(req, res){
-    Thing.find({}).then(function(things){
-       res.render("things", {
-           title: "Things",
-           activePath: "/things",
-           things: things
-       });
-    });
-});
-
-app.post("/things/new", function(req, res){
-   var thing = new Thing(req.body); 
-   thing.save(function(err){
-       if(!err){
-         req.flash("info", "A thing with an id of " + thing._id + " has been inserted");
-         res.redirect("/things"); 
-       }
-       else{
-            res.render("thing", {
-                thing: new Thing(),
-                activePath: "/things",
-                title: "Insert a New Thing",
-                error: err
-            });
-       }
-    });
-});
-
-app.post("/things/:id/delete", function(req, res, next){
-    Thing.remove({_id: req.params.id})
-        .then(function(){
-            req.flash("info", "A thing has been deleted")
-            res.redirect("/things");
-        });
-    
-});
-app.post("/things/:id", findThingById, function(req, res){
-            var thing = res.locals.thing;
-            thing.name = req.body.name;
-            thing.save(function(err, _thing){
-                if(!err){
-                    req.flash("info", "A Thing with the name " + thing.name + " has been saved");
-                    res.redirect("/things"); 
-                }
-                else {
-                    res.render("thing", {
-                       error: err,
-                       thing: thing,
-                       title: "Thing " + thing.name
-                    });  
-                }
-            });
-});
-
-app.get("/things/new", findThingById, function(req, res){
-    res.render("thing");
-    
-});
-app.get("/things/:id", findThingById, function(req, res){
-    res.render("thing");  
-});
 
 app.listen(process.env.PORT);
