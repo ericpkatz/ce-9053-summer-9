06597389c1608ca0c90c6a49962285981fb589a9
Eric Katz <ericpkatz@gmail.com>
2015-07-23 01:24:35 +0000

    use real database

--- app/config/db.js
+++ app/config/db.js
@@ -1,13 +1,16 @@
 var Q = require("q");
 var mongoose = require("mongoose");
 
+var connectionString = "mongodb://foo:bar@ds043917.mongolab.com:43917/ce-9053-summer-9";
+
 module.exports = {
     connect: connect
 };
 
 function connect(){
     var dfd = Q.defer();
-    mongoose.connect("mongodb://localhost:27017/my_world"); 
+    //mongoose.connect("mongodb://localhost:27017/my_world"); 
+    mongoose.connect(connectionString);
     mongoose.connection.on("open", function(){
         dfd.resolve();
     });
